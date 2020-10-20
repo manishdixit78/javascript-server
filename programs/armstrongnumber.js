c9503d9e7512b7ee37f731edfd0fdf1608903b59
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-10-20 13:24:55 -0700

    Docs: Add armstrongnumber.js

--- programs/armstrongnumber.js
+++ programs/armstrongnumber.js
@@ -1,22 +1,33 @@
-function toCheckArmstrong(number)
-{
-    let copy_number, sum=0;
-    copy_number=number;
+function toCheckArmstrong(number){
+    let temp;
+    let sum=0;
+    let len=number.toString().length;
+  
+    let copy_number=number;
+   while(number!=0){
+        temp=number%10;
+        find_power=Math.pow(temp,len);
+        sum=sum+find_power;
+        number=parseInt(number/10);
+   }
+   
+   if(copy_number==sum){
+       console.log(copy_number+" Is a ArmstrongNumber");
+   }
+   else{
+       console.log(copy_number+" Is not a ArmstrongNumber");
+   }
 
-    while(number!=0){
-            let rem= number%10;
-            sum= sum+(rem*rem*rem);
-            number= parseInt(number/10);
-    }
-    if(copy_number==sum){
-        console.log(copy_number+" Is a ArmstrongNumber");
-    }
-    else{
-        console.log(copy_number+" Is not a ArmstrongNumber");
-    }
 }
-toCheckArmstrong(370);
-toCheckArmstrong(200);
-toCheckArmstrong(542);
-toCheckArmstrong(407);
 toCheckArmstrong(153);
+toCheckArmstrong(407);
+toCheckArmstrong(1634); 
+toCheckArmstrong(8208); 
+toCheckArmstrong(9474);
+toCheckArmstrong(93084);
+toCheckArmstrong(548834);
+toCheckArmstrong(9926315);
+toCheckArmstrong(99);
+toCheckArmstrong(9926318);
+toCheckArmstrong(9);
+toCheckArmstrong(223);
