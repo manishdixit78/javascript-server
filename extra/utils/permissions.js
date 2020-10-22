9b840c73c457b2b6c19a1b3e83e1cbebf246369b
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-10-21 17:52:57 -0700

    Docs:Add permissions.js

---
+++ extra/utils/permissions.js
@@ -0,0 +1,41 @@
+let permissions={
+    'getUsers': {
+    all: ['head-trainer'],
+    read : ['trainee', 'trainer'],
+    write : ['trainer'],
+    delete: [],
+    },
+    'getUsers1': {
+        all: ['head-trainer'],
+        read : ['trainee', 'trainer'],
+        write : ['trainer'],
+        delete: [],
+        },
+    }
+    function hasPermission(moduleName,role,permissionType){
+     
+       if(!moduleName.hasOwnProperty(permissionType)){
+           console.log("false");
+       }
+       else if(moduleName[permissionType].includes(role)|| (role=='head-trainer')){
+           console.log("true");
+       }
+       else{
+           console.log("false");
+       }
+    }
+    hasPermission(permissions.getUsers,'head-trainer','all');
+    hasPermission(permissions.getUsers,'head-trainer','read');
+   
+    hasPermission(permissions.getUsers1,'trainer','write');
+    hasPermission(permissions.getUsers1,'trainer','delete');
+  
+
+    
+   
+   
+
+
+
+ 
+     
