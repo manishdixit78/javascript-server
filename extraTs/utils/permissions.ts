af091386ff6aff69baf6c318131bc973e4be3f3a
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-10-23 18:36:43 -0700

    Docs:Add index.ts

---
+++ extraTs/utils/permissions.ts
@@ -0,0 +1,28 @@
+// function for hasPermission for validate the Username,role and permission
+    function hasPermission(moduleName,role:string,permissionType:string):void{
+     
+       if(!moduleName.hasOwnProperty(permissionType)){
+           console.log("false");
+       }
+       else if(moduleName[permissionType].includes(role)|| (role=='head-trainer')){
+           console.log("true");
+       }
+       else{
+           console.log("false");
+       }
+    }
+   /* hasPermission(permissions.getUsers,'head-trainer','all');
+    hasPermission(permissions.getUsers,'head-trainer','read');
+    hasPermission(permissions.getUsers1,'trainer','write');
+    hasPermission(permissions.getUsers1,'trainer','delete');*/
+  
+//export {permissions};
+export {hasPermission};
+    
+   
+   
+
+
+
+ 
+     
