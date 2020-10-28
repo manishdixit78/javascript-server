293fd922d61050bf346fc7ff4b7a07d709302b75
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-10-28 16:48:07 -0700

    Add Https Handler Code

--- src/Server.ts
+++ src/Server.ts
@@ -1,29 +1,47 @@
-// create a class and define methods according to the ticket#39521
-import * as express from "express";
+// create a class and define methods according to the ticket#39522
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { notFoundHandler, errorHandler } from './libs/routes';
+import notFoundRoute from "./libs/routes/notFoundRoute";
 class Server {
-app
-constructor( private config ) {
-this.app=express()
-}
-bootstrap() {
-this.setupRouts()
-return this;
-}
-setupRouts() {
-const { app }=this;
-app.get('/health-check',( req, res, next ) => {
-res.send("I am fine");
-});
-return this;
-}
-run() {
-const {app, config:{ PORT }}=this;
-app.listen(PORT,( err ) => {
-if( err ) {
-console.log( err );
-}
-console.log(`App is running on port ${ PORT }`);
-})
-}
+    app
+    constructor(private config) {
+        this.app = express()
+    }
+    bootstrap() {
+        this.setupRouts();
+        this.initBodyParser();
+        return this;
+    }
+    setupRouts() {
+        const { app } = this;
+
+        app.use ((req, res, next) => {
+            console.log('Inside First MidleWare');
+            next()
+        });
+
+        app.use('/health-check', (req, res) => {
+            console.log('Inside Second MidleWare');
+            res.send('I am fine');
+        });
+
+        app.use(notFoundHandler);
+        app.use(errorHandler);
+
+        return this;
+    }
+    initBodyParser() {
+        this.app.use(bodyParser.json({ type: 'application/*+json' }))
+    }
+    run() {
+        const { app, config: { PORT } } = this;
+        app.listen(PORT, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(`App is running on port ${PORT}`);
+        })
+    }
 }
 export default Server;
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,27 @@
 //import the server and config files
 import Server from './Server';
 import config from './config/configuration';
-console.log("config is " , config);
-const server=new Server( { PORT :9000 } );
+console.log("config is ", config);
+const server = new Server({ PORT: 9000 });
 server.bootstrap().run();
 
 /*                Task
-Define .env file with following properties:
+Create a method initBodyParser in Server.ts file
+This will implement the body parser for the express app.
+Please refer the URL for documentation(https://www.npmjs.com/package/body-parser#examples)
+Call this method from the bootstrap method of the same class.
 
-PORT=9000
-NODE_ENV=dev
+Create folder routes inside libs
 
-Create a folder src
+Create a file notFoundRoute.ts
+Create a middleware which will return error 'Not Found'.
 
-Create a file Server.ts inside src
-
-Define a class with the following methods and properties:
-
-- config: property, It will be set by constructor from parameter received.
-- bootstrap: call different methods needed for bootstrapping of app.
-It will call setupRoutes for now.
-It will return this.
-- setupRoutes: Method to setup routes
-We will define a route(of type get) '/health-check' which will return 'I am OK' as response.
-- run: Method to run the express app.
-Log error in case of error
-Log success message in case of success.
-It will return this.
-
-Export Server class.
-
-Create a folder config inside src
-- Create a file IConfig.ts which contains interface of config (properties like port and env)
-- Create a file configuration.ts
-Use dotenv module for extracting config from .env file.
-Define config using interface(IConfig).
-Use Object.freeze for protecting config object against any modifications after the declaration.
-Export config
-
-Create a file index.ts
-Import config and Server.
-Create an instance of Server by passing config in constructor
-Call methods bootstrap and run using instance
-App should be running and success message.
+Create a file errorHandler.ts
+Create a middleware which returns an error in the following format:
+{
+error: "Not Found",
+message: "error",
+status: 500,
+timestamp: "2019-01-08T17:38:21.929Z"
+}
 */
