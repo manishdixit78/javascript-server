3643b02b54a0946abb8d9834630cbf8c7abdd492
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-11-03 18:46:20 -0800

    Add authMoiddleware.ts file

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -3,10 +3,11 @@ import { Router } from "express";
 import TraineeController from "./Controller";
 import validationHandler from '../../libs/validationHandler';
 import validation from './validation';
+import { authMiddlware } from '../../libs/routes';
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-    .get(validationHandler(validation.get), TraineeController.get)
+    .get(authMiddlware('getUser','all'), validationHandler(validation.get), TraineeController.get)
     .post(validationHandler(validation.create), TraineeController.create)
     .put(validationHandler(validation.update), TraineeController.update)
     .delete(validationHandler(validation.delete), TraineeController.delete);
