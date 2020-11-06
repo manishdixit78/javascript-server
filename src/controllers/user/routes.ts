f8c9de1837d93032611dd692a3249efbc0f26e3e
manishdixit78 <72734557+manishdixit78@users.noreply.github.com>
2020-11-06 10:52:39 -0800

    Add Validation.ts

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -1,13 +1,15 @@
 // create a routes.ts file
 import { Router } from "express";
 import UserController from "./Controller";
+import validationHandler from '../../libs/validationHandler';
+import validation from './validation';
 
 const UserRouter = Router();
 
 UserRouter.route('/')
-    .get( UserController.get)
-    .post( UserController.create)
-    .put( UserController.update)
-    .delete( UserController.delete);
+    .get( validationHandler(validation.get), UserController.get)
+    .post( validationHandler(validation.create), UserController.create)
+    .put( validationHandler(validation.update), UserController.update)
+    .delete( validationHandler(validation.delete), UserController.delete); 
 
 export default UserRouter;
